e7d9d91c813118f0101fee3443af52fb94dc7d77
Markus Tzoe <teamo@live.com>
2015-08-17 18:56:50 +0800

    explicit Encoder/Decoder definition

--- decoder.go
+++ decoder.go
@@ -5,7 +5,25 @@ import (
 	"os"
 )
 
-var SSE_Enabled bool
+type Decoder struct {
+	codec        [MAX_NCH]tta_codec // 1 per channel
+	channels     int                // number of channels/codecs
+	data         [8]byte            // codec initialization data
+	fifo         tta_fifo
+	password_set bool     // password protection flag
+	seek_allowed bool     // seek table flag
+	seek_table   []uint64 // the playing position table
+	format       uint32   // tta data format
+	rate         uint32   // bitrate (kbps)
+	offset       uint64   // data start position (header size, bytes)
+	frames       uint32   // total count of frames
+	depth        uint32   // bytes per sample
+	flen_std     uint32   // default frame length in samples
+	flen_last    uint32   // last frame length in samples
+	flen         uint32   // current frame length in samples
+	fnum         uint32   // currently playing frame index
+	fpos         uint32   // the current position in frame
+}
 
 func Decompress(infile, outfile *os.File, passwd string, cb Callback) (err error) {
 	decoder := NewDecoder(infile)
--- tta.go
+++ tta.go
@@ -60,43 +60,6 @@ type tta_fifo struct {
 	io     io.ReadWriteSeeker
 }
 
-type Decoder struct {
-	codec        [MAX_NCH]tta_codec // 1 per channel
-	channels     int                // number of channels/codecs
-	data         [8]byte            // codec initialization data
-	fifo         tta_fifo
-	password_set bool     // password protection flag
-	seek_allowed bool     // seek table flag
-	seek_table   []uint64 // the playing position table
-	format       uint32   // tta data format
-	rate         uint32   // bitrate (kbps)
-	offset       uint64   // data start position (header size, bytes)
-	frames       uint32   // total count of frames
-	depth        uint32   // bytes per sample
-	flen_std     uint32   // default frame length in samples
-	flen_last    uint32   // last frame length in samples
-	flen         uint32   // current frame length in samples
-	fnum         uint32   // currently playing frame index
-	fpos         uint32   // the current position in frame
-}
-
-type Encoder struct {
-	codec      [MAX_NCH]tta_codec // 1 per channel
-	channels   int                // number of channels/codecs
-	data       [8]byte            // codec initialization data
-	fifo       tta_fifo
-	seek_table []uint64 // the playing position table
-	format     uint32   // tta data format
-	rate       uint32   // bitrate (kbps)
-	offset     uint64   // data start position (header size, bytes)
-	frames     uint32   // total count of frames
-	depth      uint32   // bytes per sample
-	flen_std   uint32   // default frame length in samples
-	flen_last  uint32   // last frame length in samples
-	flen       uint32   // current frame length in samples
-	fnum       uint32   // currently playing frame index
-	fpos       uint32   // the current position in frame
-	shift_bits uint32   // packing int to pcm
-}
-
 type Callback func(uint32, uint32, uint32)
+
+var SSE_Enabled bool
